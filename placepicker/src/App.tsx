35f3f153503ac83c3cf6aac4bfa792af2aaccbd4
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2023-10-26 02:13:17 +1100

    modified app.tsx code and index.css

--- placepicker/src/App.tsx
+++ placepicker/src/App.tsx
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useCallback } from "react";
+import { Fragment } from "react";
+
+import Places from "./components/Places";
+import Modal from "./components/Modal";
+import DeleteConfirmation from "./components/DeleteConfirmation";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces";
+
+import { Place } from "./components/Places.jsx";
 
 function App() {
+  const selectedPlace = useRef<Place | null>(null);
+
+  const [userPlaces, setUserPlaces] = useState<Place[]>([]);
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+
+  const handleStartRemovePlace = (place: Place) => {
+    setModalIsOpen(true);
+    selectedPlace.current = place;
+  };
+
+  const handleStopRemovePlace = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleSelectPlace = (selectedPlace: Place) => {
+    setUserPlaces((prevPickedPlaces) => {
+      if (!prevPickedPlaces) {
+        prevPickedPlaces = [];
+      }
+      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+        return prevPickedPlaces;
+      }
+      return [selectedPlace, ...prevPickedPlaces];
+    });
+  };
+
+  const handleRemovePlace = useCallback(async function handleRemovePlace() {
+    setUserPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current?.id)
+    );
+
+    setModalIsOpen(false);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Fragment>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Create your personal collection of places you would like to visit or
+          you have visited.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText="Select the places you would like to visit below."
+          places={userPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+
+        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+      </main>
+    </Fragment>
   );
 }
 
