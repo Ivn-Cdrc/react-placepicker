d822fee0ffd8487fcca5d1e23a96f96242b422b9
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2023-10-28 00:27:22 +1100

    initialised spring rest api for the backend

--- placepicker/src/components/AvailablePlaces.tsx
+++ placepicker/src/components/AvailablePlaces.tsx
@@ -1,15 +1,31 @@
+import { useState, useEffect } from "react";
+
 import Places from "./Places";
 import { Place } from "./Places";
 
-const AvailablePlaces = ({
-  onSelectPlace,
-}: {
+interface AvailablePlacesProps {
   onSelectPlace: (place: Place) => void;
-}) => {
+}
+
+const AvailablePlaces = ({ onSelectPlace }: AvailablePlacesProps) => {
+  // must first render the component without data, then render it again once
+  // data has been fetched
+  const [availablePlaces, setAvailablePlaces] = useState<Place[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:2999/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setAvailablePlaces(responseData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
