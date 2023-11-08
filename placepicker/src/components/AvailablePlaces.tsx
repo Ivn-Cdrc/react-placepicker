b2bf303ae52c880ded3406810b802faf660e5fe5
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2023-11-08 20:23:55 +1100

    added management of loading states

--- placepicker/src/components/AvailablePlaces.tsx
+++ placepicker/src/components/AvailablePlaces.tsx
@@ -11,21 +11,27 @@ const AvailablePlaces = ({ onSelectPlace }: AvailablePlacesProps) => {
   // must first render the component without data, then render it again once
   // data has been fetched
   const [availablePlaces, setAvailablePlaces] = useState<Place[]>([]);
+  // isFetching is used to handle loading states while fetching information
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:8080/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        setAvailablePlaces(responseData);
-      });
+    const fetchPlaces = async () => {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- placepicker/src/components/Places.tsx
+++ placepicker/src/components/Places.tsx
@@ -14,6 +14,8 @@ export interface Place {
 interface PlacesProps {
   title: string;
   places: Place[];
+  isLoading?: boolean;
+  loadingText?: string;
   fallbackText: string;
   onSelectPlace: (place: Place) => void;
 }
@@ -21,14 +23,19 @@ interface PlacesProps {
 const Places = ({
   title,
   places,
+  isLoading,
+  loadingText,
   fallbackText,
   onSelectPlace,
 }: PlacesProps) => {
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {(places && places.length === 0) && <p className="fallback-text">{fallbackText}</p>}
-      {(places && places.length > 0) && (
+      {isLoading && <p className="fallback-text">{loadingText}</p>}
+      {!isLoading && places && places.length === 0 && (
+        <p className="fallback-text">{fallbackText}</p>
+      )}
+      {(!isLoading && places && places.length > 0) && (
         <ul className="places">
           {places.map((place) => (
             <li key={place.id} className="place-item">
